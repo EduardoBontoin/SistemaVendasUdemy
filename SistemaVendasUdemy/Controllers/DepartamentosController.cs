2c33b5eaaae08a8c8a7f8026b2bf7ddea6ff2547
EduardoBontorin <eduardo.bontorin@outlook.com>
2023-02-22 16:47:33 -0300

    Primeiro Model-Controller-View

---
+++ SistemaVendasUdemy/Controllers/DepartamentosController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaVendasUdemy.Models;
+
+namespace SistemaVendasUdemy.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> list = new List<Departamento>();
+            list.Add(new Departamento { Id = 1, Name = "Eletronics" });
+            list.Add(new Departamento { Id = 2, Name = "Fashion" });
+
+            return View((list));
+        }
+    }
+}
